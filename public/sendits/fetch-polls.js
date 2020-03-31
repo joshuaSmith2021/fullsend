02629b505b7b4718a908a40e6427c66ffbd6fe34
Joshua Smith <32210099+joshuaSmith2021@users.noreply.github.com>
2020-03-31 14:58:25 -0700

    Update fetch-polls.js

--- public/sendits/fetch-polls.js
+++ public/sendits/fetch-polls.js
@@ -1,5 +1,8 @@
 const pollList = document.getElementById('displayPolls');
 
+let userPolls = [];
+let desiredLength = null;
+
 function copyToClipboard (str) {
 	const el = document.createElement('textarea');
 	el.value = str;
@@ -41,6 +44,55 @@ function buildListItem(question, creation, docId, i) {
 	return `<li class="mdl-list__item mdl-list__item--three-line"><span class="mdl-list__item-primary-content"><i class="material-icons mdl-list__item-avatar">poll</i><span>Ask me something!</span><span class="mdl-list__item-text-body">Created on ${fancyDate.calendarDay} at ${fancyDate.timeString}</span></span><span class="mdl-list__item-secondary-content"><a class="mdl-list__item-secondary-action" id="tt${i}"><i class="material-icons copyLinkButton" data-docId="${docId}">link</i></a><div class="mdl-tooltip" data-mdl-for="tt${i}">Copy Share Link</div></span></li>`;
 }
 
+function init() {
+	// Get data from firestore
+	const user = firebase.auth().currentUser;
+	const uid  = user.uid;
+	const firestore = firebase.firestore();
+
+	firestore.collection('users').doc(uid).get().then(doc => {
+		let data = doc.data();
+		let pollIds = data.sendits;
+
+		if (typeof pollIds !== "object") {
+			pollIds = null;
+		} else if (pollIds.length === 0) {
+			pollIds = null;
+		}
+
+		if (pollIds == null) {
+			// Create a no polls message or something, idk
+		} else {
+			desiredLength = pollIds.length;
+			for (let i = 0; i < pollIds.length; i++) {
+				let currentId = pollIds[i];
+				firestore.collection('sendits').doc(currentId).get().then(document => {
+					let pollData = document.data();
+
+					userPolls.append({
+						question : pollData.question,
+						timestamp: polldata.created,
+						username : polldata.creator
+					});
+				}).catch(err => {
+					console.error(err);
+				});
+			}
+
+			const checkForCompletion = setInterval(function() {
+				if (userPolls.length === desiredLength) {
+					clearInterval(checkForCompletion);
+					console.log(userPolls);
+				}
+			}
+		}
+
+		userPolls = pollIds;
+	}).catch(err => {
+		console.error(err);
+	});
+}
+
 function renderList() {
 	// add list items for each poll to document
 }
@@ -63,55 +115,4 @@ for (let i = 0; i < copyButtons.length; i++) {
 			message: "Link copied to clipboard!"
 		});
 	});
-}
-
-// Get data from firestore
-const user = firebase.auth().currentUser;
-const uid  = user.uid;
-const firestore = firebase.firestore();
-
-let userPolls = [];
-let desiredLength = null;
-
-function init() {
-firestore.collection('users').doc(uid).get().then(doc => {
-	let data = doc.data();
-        let pollIds = data.sendits;
-
-        if (typeof pollIds !== "object") {
-                pollIds = null;
-        } else if (pollIds.length === 0) {
-                pollIds = null;
-        }
-
-	if (pollIds == null) {
-		// Create a no polls message or something, idk
-	} else {
-		desiredLength = pollIds.length;
-		for (let i = 0; i < pollIds.length; i++) {
-			let currentId = pollIds[i];
-			firestore.collection('sendits').doc(currentId).get().then(document => {
-				let pollData = document.data();
-
-				userPolls.append({
-					question : pollData.question,
-					timestamp: polldata.created,
-					username : polldata.creator
-				});
-			}).catch(err => {
-				console.error(err);
-			});
-		}
-
-		const checkForCompletion = setInterval(function() {
-                	if (userPolls.length === desiredLength) {
-				clearInterval(checkForCompletion);
-				console.log(userPolls);
-		}
-	}
-
-	userPolls = pollIds;
-}).catch(err => {
-	console.error(err);
-});
 }
