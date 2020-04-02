d594d04ae5f13162a8eeb7db95163eef47f78dc6
joshuaSmith2021 <joshuasmith6556@gmail.com>
2020-04-01 18:34:28 -0700

    yhlqmdlg

--- public/create/create-poll.js
+++ public/create/create-poll.js
@@ -10,6 +10,29 @@ document.querySelector('#createTrigger').addEventListener('click', function() {
 
 	let questionInput = document.querySelector('#questionInput');
 	let questionText  = questionInput.value;
+
+	let documentName = uuidv4();
+
+	firestore.collection('sendits').doc(documentName).set({
+		created : new Date(),
+		creator : '',
+		question: questionText 
+	}).then(() => {
+		console.log('Sendit created successfully!');
+		// Update user document
+		let currentUser = firebase.auth().currentUser;
+		firestore.collection('users').doc(currentUser.uid).update({
+			sendits: firebase.firestore.FieldValue.arrayUnion(documentName)
+		}).then(() => {
+			//location.replace('/sendits/');
+			console.log(`added ${documentName} to users/${currentUser}`);
+		}).catch(err => {
+			console.error(err);
+			console.error('Failed to update user document');
+		});
+	}).catch(err => {
+		console.error(err);
+	});
 });
 
 firebase.auth().onAuthStateChanged(function(u) {
